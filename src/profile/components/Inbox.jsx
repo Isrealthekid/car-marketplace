14ff0ec32ea344a931915f602c7ff76a1a25d3af
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-29 13:10:36 +0100

    inboxDiv

--- src/profile/components/Inbox.jsx
+++ src/profile/components/Inbox.jsx
@@ -6,13 +6,17 @@ const Inbox = () => {
   return (
     <div>
         <div style={{ width:'100vw', height:'100vh' }}>
-      <SendbirdApp
+      {/* <SendbirdApp
         // You can find your Sendbird application ID on the Sendbird dashboard. 
-        appId={'import.meta.env.VITE_SENDBIRD_APP_ID'}
+        // appId={'import.meta.env.VITE_SENDBIRD_APP_ID'}
+        appId={'F3F6F9C5-0C9D-4A3D-9D9A-0A2C2D0E9F0D'}
         // Specify the user ID you've created on the dashboard.
-        // Or you can create a user by specifying a unique userId.  
-        userId={'FOWOSERE'}
-      />
+        
+        // Or you can create a user by specifying a unique userId.        
+   
+        userId={'isreal'}
+        
+      /> */}
     </div>
     </div>
   )
